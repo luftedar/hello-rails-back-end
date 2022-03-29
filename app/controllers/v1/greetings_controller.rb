0c66b6c38980a1e420afebaae56708bc1cdbe1f0
luftedar <ugurorcun91@gmail.com>
2022-03-29 16:13:14 +0300

    Fix Greetings Controller Render Json

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class V1::GreetingsController < ApplicationController
   def index
-    message = Message.all.sample
-    render json { message: }.to_json
+    message = Greeting.all.sample
+    render json: { message: }.to_json
   end
 end
