ed9f453ee546b10fafe47c238455c8f1f57c49b0
luftedar <ugurorcun91@gmail.com>
2022-03-29 15:53:41 +0300

    Generate Api

---
+++ app/controllers/v1/greetings_controller.rb
@@ -0,0 +1,4 @@
+class V1::GreetingsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :v1 do
+    get 'greetings/index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
