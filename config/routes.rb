95d44b69106470768f6dc0e973931379c01ce672
luftedar <ugurorcun91@gmail.com>
2022-03-29 16:13:52 +0300

    Add Default Option for the V1 Api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    get 'greetings/index'
+  namespace :v1, defaults: { format: 'json' } do
+    get '/greetings', to: 'greetings#index'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
