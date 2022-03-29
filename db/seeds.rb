11adc96e332112c8a3ee7dac13e225127ae6ef25
luftedar <ugurorcun91@gmail.com>
2022-03-29 15:58:31 +0300

    Add Five Different Greetings in Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(messages: 'Hello')
+Greeting.create(messages: 'Merhaba')
+Greeting.create(messages: 'How are you?')
+Greeting.create(messages: 'Nasilsin?')
+Greeting.create(messages: 'Naptin?')
